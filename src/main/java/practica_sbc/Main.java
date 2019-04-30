efb8ac3f7650a076302a361c3d2cbdd453cfbb59
Nicolae Alexe <nicolaealexe@MacBook-Pro-de-Nicolae.local>
2019-04-30 20:18:10 +0200

    creadas algunas relaciones basicas e integrada nueva funcion refactorizada para mapear instancias

--- src/main/java/practica_sbc/Main.java
+++ src/main/java/practica_sbc/Main.java
@@ -14,12 +14,17 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDataPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLDataPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLDataRange;
 import org.semanticweb.owlapi.model.OWLDatatype;
+import org.semanticweb.owlapi.model.OWLDatatypeRestriction;
 import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
 import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
@@ -28,167 +33,90 @@ import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 import org.semanticweb.owlapi.model.PrefixManager;
 import org.semanticweb.owlapi.util.DefaultPrefixManager;
+import org.semanticweb.owlapi.vocab.OWLFacet;
 
 import java.util.*;
 
 public class Main {
-	static final OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-	static final OWLDataFactory factory = manager.getOWLDataFactory();
 	static String dbpedia = "http://dbpedia.org/sparql";
 	static String wikidata = "https://query.wikidata.org/sparql";
 	static String mondial = "http://servolis.irisa.fr/mondial/sparql";
-	@Nonnull
-	private static final String KOALA = "<?xml version=\"1.0\"?>\n"
-			+ "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\" xmlns:owl=\"http://www.w3.org/2002/07/owl#\" xmlns=\"http://protege.stanford.edu/plugins/owl/owl-library/koala.owl#\" xml:base=\"http://protege.stanford.edu/plugins/owl/owl-library/koala.owl\">\n"
-			+ "  <owl:Ontology rdf:about=\"\"/>\n"
-			+ "  <owl:Class rdf:ID=\"Female\"><owl:equivalentClass><owl:Restriction><owl:onProperty><owl:FunctionalProperty rdf:about=\"#hasGender\"/></owl:onProperty><owl:hasValue><Gender rdf:ID=\"female\"/></owl:hasValue></owl:Restriction></owl:equivalentClass></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Marsupials\"><owl:disjointWith><owl:Class rdf:about=\"#Person\"/></owl:disjointWith><rdfs:subClassOf><owl:Class rdf:about=\"#Animal\"/></rdfs:subClassOf></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Student\"><owl:equivalentClass><owl:Class><owl:intersectionOf rdf:parseType=\"Collection\"><owl:Class rdf:about=\"#Person\"/><owl:Restriction><owl:onProperty><owl:FunctionalProperty rdf:about=\"#isHardWorking\"/></owl:onProperty><owl:hasValue rdf:datatype=\"http://www.w3.org/2001/XMLSchema#boolean\">true</owl:hasValue></owl:Restriction><owl:Restriction><owl:someValuesFrom><owl:Class rdf:about=\"#University\"/></owl:someValuesFrom><owl:onProperty><owl:ObjectProperty rdf:about=\"#hasHabitat\"/></owl:onProperty></owl:Restriction></owl:intersectionOf></owl:Class></owl:equivalentClass></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"KoalaWithPhD\"><owl:versionInfo>1.2</owl:versionInfo><owl:equivalentClass><owl:Class><owl:intersectionOf rdf:parseType=\"Collection\"><owl:Restriction><owl:hasValue><Degree rdf:ID=\"PhD\"/></owl:hasValue><owl:onProperty><owl:ObjectProperty rdf:about=\"#hasDegree\"/></owl:onProperty></owl:Restriction><owl:Class rdf:about=\"#Koala\"/></owl:intersectionOf></owl:Class></owl:equivalentClass></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"University\"><rdfs:subClassOf><owl:Class rdf:ID=\"Habitat\"/></rdfs:subClassOf></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Koala\"><rdfs:subClassOf><owl:Restriction><owl:hasValue rdf:datatype=\"http://www.w3.org/2001/XMLSchema#boolean\">false</owl:hasValue><owl:onProperty><owl:FunctionalProperty rdf:about=\"#isHardWorking\"/></owl:onProperty></owl:Restriction></rdfs:subClassOf><rdfs:subClassOf><owl:Restriction><owl:someValuesFrom><owl:Class rdf:about=\"#DryEucalyptForest\"/></owl:someValuesFrom><owl:onProperty><owl:ObjectProperty rdf:about=\"#hasHabitat\"/></owl:onProperty></owl:Restriction></rdfs:subClassOf><rdfs:subClassOf rdf:resource=\"#Marsupials\"/></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Animal\"><rdfs:seeAlso>Male</rdfs:seeAlso><rdfs:subClassOf><owl:Restriction><owl:onProperty><owl:ObjectProperty rdf:about=\"#hasHabitat\"/></owl:onProperty><owl:minCardinality rdf:datatype=\"http://www.w3.org/2001/XMLSchema#int\">1</owl:minCardinality></owl:Restriction></rdfs:subClassOf><rdfs:subClassOf><owl:Restriction><owl:cardinality rdf:datatype=\"http://www.w3.org/2001/XMLSchema#int\">1</owl:cardinality><owl:onProperty><owl:FunctionalProperty rdf:about=\"#hasGender\"/></owl:onProperty></owl:Restriction></rdfs:subClassOf><owl:versionInfo>1.1</owl:versionInfo></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Forest\"><rdfs:subClassOf rdf:resource=\"#Habitat\"/></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Rainforest\"><rdfs:subClassOf rdf:resource=\"#Forest\"/></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"GraduateStudent\"><rdfs:subClassOf><owl:Restriction><owl:onProperty><owl:ObjectProperty rdf:about=\"#hasDegree\"/></owl:onProperty><owl:someValuesFrom><owl:Class><owl:oneOf rdf:parseType=\"Collection\"><Degree rdf:ID=\"BA\"/><Degree rdf:ID=\"BS\"/></owl:oneOf></owl:Class></owl:someValuesFrom></owl:Restriction></rdfs:subClassOf><rdfs:subClassOf rdf:resource=\"#Student\"/></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Parent\"><owl:equivalentClass><owl:Class><owl:intersectionOf rdf:parseType=\"Collection\"><owl:Class rdf:about=\"#Animal\"/><owl:Restriction><owl:onProperty><owl:ObjectProperty rdf:about=\"#hasChildren\"/></owl:onProperty><owl:minCardinality rdf:datatype=\"http://www.w3.org/2001/XMLSchema#int\">1</owl:minCardinality></owl:Restriction></owl:intersectionOf></owl:Class></owl:equivalentClass><rdfs:subClassOf rdf:resource=\"#Animal\"/></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"DryEucalyptForest\"><rdfs:subClassOf rdf:resource=\"#Forest\"/></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Quokka\"><rdfs:subClassOf><owl:Restriction><owl:hasValue rdf:datatype=\"http://www.w3.org/2001/XMLSchema#boolean\">true</owl:hasValue><owl:onProperty><owl:FunctionalProperty rdf:about=\"#isHardWorking\"/></owl:onProperty></owl:Restriction></rdfs:subClassOf><rdfs:subClassOf rdf:resource=\"#Marsupials\"/></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"TasmanianDevil\"><rdfs:subClassOf rdf:resource=\"#Marsupials\"/></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"MaleStudentWith3Daughters\"><owl:equivalentClass><owl:Class><owl:intersectionOf rdf:parseType=\"Collection\"><owl:Class rdf:about=\"#Student\"/><owl:Restriction><owl:onProperty><owl:FunctionalProperty rdf:about=\"#hasGender\"/></owl:onProperty><owl:hasValue><Gender rdf:ID=\"male\"/></owl:hasValue></owl:Restriction><owl:Restriction><owl:onProperty><owl:ObjectProperty rdf:about=\"#hasChildren\"/></owl:onProperty><owl:cardinality rdf:datatype=\"http://www.w3.org/2001/XMLSchema#int\">3</owl:cardinality></owl:Restriction><owl:Restriction><owl:allValuesFrom rdf:resource=\"#Female\"/><owl:onProperty><owl:ObjectProperty rdf:about=\"#hasChildren\"/></owl:onProperty></owl:Restriction></owl:intersectionOf></owl:Class></owl:equivalentClass></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Degree\"/>\n  <owl:Class rdf:ID=\"Gender\"/>\n"
-			+ "  <owl:Class rdf:ID=\"Male\"><owl:equivalentClass><owl:Restriction><owl:hasValue rdf:resource=\"#male\"/><owl:onProperty><owl:FunctionalProperty rdf:about=\"#hasGender\"/></owl:onProperty></owl:Restriction></owl:equivalentClass></owl:Class>\n"
-			+ "  <owl:Class rdf:ID=\"Person\"><rdfs:subClassOf rdf:resource=\"#Animal\"/><owl:disjointWith rdf:resource=\"#Marsupials\"/></owl:Class>\n"
-			+ "  <owl:ObjectProperty rdf:ID=\"hasHabitat\"><rdfs:range rdf:resource=\"#Habitat\"/><rdfs:domain rdf:resource=\"#Animal\"/></owl:ObjectProperty>\n"
-			+ "  <owl:ObjectProperty rdf:ID=\"hasDegree\"><rdfs:domain rdf:resource=\"#Person\"/><rdfs:range rdf:resource=\"#Degree\"/></owl:ObjectProperty>\n"
-			+ "  <owl:ObjectProperty rdf:ID=\"hasChildren\"><rdfs:range rdf:resource=\"#Animal\"/><rdfs:domain rdf:resource=\"#Animal\"/></owl:ObjectProperty>\n"
-			+ "  <owl:FunctionalProperty rdf:ID=\"hasGender\"><rdfs:range rdf:resource=\"#Gender\"/><rdf:type rdf:resource=\"http://www.w3.org/2002/07/owl#ObjectProperty\"/><rdfs:domain rdf:resource=\"#Animal\"/></owl:FunctionalProperty>\n"
-			+ "  <owl:FunctionalProperty rdf:ID=\"isHardWorking\"><rdfs:range rdf:resource=\"http://www.w3.org/2001/XMLSchema#boolean\"/><rdfs:domain rdf:resource=\"#Person\"/><rdf:type rdf:resource=\"http://www.w3.org/2002/07/owl#DatatypeProperty\"/></owl:FunctionalProperty>\n"
-			+ "  <Degree rdf:ID=\"MA\"/>\n</rdf:RDF>";
-
-	static public ArrayList<String> getPaises() {
-		String countryQuery = "PREFIX n1: <http://www.semwebtech.org/mondial/10/meta#>"
-				+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>" + "SELECT DISTINCT *" + "WHERE { ?country a n1:Country ."
-				+ " ?country n1:name ?name_21 ." + "}" + "LIMIT 200";
-		QueryExecution qexec = QueryExecutionFactory.sparqlService("http://servolis.irisa.fr/mondial/sparql",
-				countryQuery); // Query
-
-		ResultSet results = qexec.execSelect();
-		ArrayList<String> paises = new ArrayList<String>();
-		while (results.hasNext()) {
-			QuerySolution soln = results.nextSolution();
-			paises.add(soln.getLiteral("name_21").toString());
-		}
-		return paises;
-	}
-
-	static public ArrayList<String> getActores() {
-		String actoresQuery = "PREFIX dbo: <http://dbpedia.org/ontology/>\n"
-				+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>" + "SELECT DISTINCT ?Actor_1 ?nombre \n"
-				+ "WHERE { ?Actor_1 a dbo:Actor .\n" + "?Actor_1 foaf:name ?nombre."
-				+ "        FILTER ( NOT EXISTS { ?Actor_1 dbo:activeYearsEndYear ?activeYearsEndYear_21 . } ) }\n"
-				+ "LIMIT 200";
-
-		Query query = QueryFactory.create(actoresQuery);
-		QueryExecution qexec = QueryExecutionFactory.sparqlService(dbpedia, query);
-
-		ResultSet results = qexec.execSelect();
-		ArrayList<String> actores = new ArrayList<String>();
-		while (results.hasNext()) {
-			QuerySolution soln = results.nextSolution();
-			actores.add(formatString(soln.getLiteral("nombre").toString()));
-		}
-		qexec.close();
-		return actores;
-	}
+	static String actoresQuery = "PREFIX dbo: <http://dbpedia.org/ontology/>\n"
+			+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>" + "SELECT DISTINCT ?Actor_1 ?nombre \n"
+			+ "WHERE { ?Actor_1 a dbo:Actor .\n" + "?Actor_1 foaf:name ?nombre."
+			+ "        FILTER ( NOT EXISTS { ?Actor_1 dbo:activeYearsEndYear ?activeYearsEndYear_21 . } ) }\n"
+			+ "LIMIT 200";
+	static String countryQuery = "PREFIX n1: <http://www.semwebtech.org/mondial/10/meta#>"
+			+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>" + "SELECT DISTINCT *" + "WHERE { ?country a n1:Country ."
+			+ " ?country n1:name ?name_21 ." + "}" + "LIMIT 200";
+	static String filmQueryString = "PREFIX wd: <http://www.wikidata.org/entity/>"
+			+ "PREFIX wdt: <http://www.wikidata.org/prop/direct/>" + "PREFIX wikibase: <http://wikiba.se/ontology#>"
+			+ "PREFIX p: <http://www.wikidata.org/prop/>" + "PREFIX v: <http://www.wikidata.org/prop/statement/>"
+			+ "PREFIX q: <http://www.wikidata.org/prop/qualifier/>"
+			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+			+ "SELECT DISTINCT ?q ?film_title ?actor ?genre" + "WHERE {" + " ?q wdt:P31 wd:Q11424."
+			+ "?q rdfs:label ?film_title filter (lang(?film_title) = \"en\")." + "?q wdt:P136 ?genreID."
+			+ "?genreID rdfs:label ?genre filter (lang(?genre) = \"en\")." + "?q wdt:P161 ?actorID."
+			+ " ?actorID rdfs:label ?actor filter (lang(?actor) = \"en\")." + "}limit 200";
 	
-	static public void mappingInstances (OWLClass ontologyClass, ArrayList<String> instances, PrefixManager dbpm, OWLOntology ontology, String type) {
-		for (int i = 0; i < instances.size(); i++) {
-			String name = instances.get(i);
-			OWLNamedIndividual instance = factory.getOWLNamedIndividual("#" + name.toString(), dbpm);
-			if(type.equals("actores")){
-				OWLClassAssertionAxiom isActor = factory.getOWLClassAssertionAxiom(ontologyClass, instance);
-				manager.addAxiom(ontology, isActor);
-			} else if(type.equals("peliculas")) {
-				OWLClassAssertionAxiom isPelicula = factory.getOWLClassAssertionAxiom(ontologyClass, instance);
-				manager.addAxiom(ontology, isPelicula);
-			} else if(type.equals("paises")){
-				OWLClassAssertionAxiom isPais = factory.getOWLClassAssertionAxiom(ontologyClass, instance);
-				manager.addAxiom(ontology, isPais);
-			}
-			
-		}
-		
+	static public ResultSet getData(String queryString, String service) {
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(service, query); // Query
+		ResultSet results = qexec.execSelect();
+		return results;
 	}
-	
-	static public String formatString (String stringToFormat){
+
+
+	static public String formatString(String stringToFormat) {
 		return stringToFormat.split("@en")[0];
 	}
 
-	static public ArrayList<String> getPeliculas() {
-		String filmQueryString = "PREFIX wd: <http://www.wikidata.org/entity/>"
-				+ "PREFIX wdt: <http://www.wikidata.org/prop/direct/>" + "PREFIX wikibase: <http://wikiba.se/ontology#>"
-				+ "PREFIX p: <http://www.wikidata.org/prop/>" + "PREFIX v: <http://www.wikidata.org/prop/statement/>"
-				+ "PREFIX q: <http://www.wikidata.org/prop/qualifier/>"
-				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-				+ "SELECT DISTINCT ?q ?film_title ?actor ?genre" + "WHERE {" + " ?q wdt:P31 wd:Q11424."
-				+ "?q rdfs:label ?film_title filter (lang(?film_title) = \"en\")." + "?q wdt:P136 ?genreID."
-				+ "?genreID rdfs:label ?genre filter (lang(?genre) = \"en\")." + "?q wdt:P161 ?actorID."
-				+ " ?actorID rdfs:label ?actor filter (lang(?actor) = \"en\")." + "}limit 200";
-		Query filmQuery = QueryFactory.create(filmQueryString);
-		QueryExecution qexec = QueryExecutionFactory.sparqlService("https://query.wikidata.org/sparql", filmQuery); // Query
-		ResultSet results = qexec.execSelect();
-		ArrayList<String> peliculas = new ArrayList<String>();
-		while (results.hasNext()) {
-			QuerySolution soln = results.nextSolution();
-			peliculas.add(formatString(soln.getLiteral("film_title").toString()));
-		}
-		return peliculas;
-
-	}
 
 	static public void main(String... argv) throws OWLOntologyStorageException, OWLOntologyCreationException {
 		File directory = new File(".");
 		File file = new File(directory + File.separator + "ontology.owl");
+		OntologyManager manager = new OntologyManager();
+		OWLDataFactory factory = manager.getFactory();
+		OWLOntology ontology = manager.getOntology();
 		PrefixManager dbpm = new DefaultPrefixManager("<http://dbpedia.org/ontology/>");
 
 		// OWLOntology onto = manager.loadOntologyFromOntologyDocument(file);
-		OWLClass actorClass = factory.getOWLClass("Actor");
-		OWLClass peliculaClass = factory.getOWLClass("Pelicula");
-		OWLClass paisClass = factory.getOWLClass("Pais");
-		OWLObjectProperty hacePeliculas = factory.getOWLObjectProperty("hacePeliculas");
-		OWLObjectPropertyDomainAxiom rangeAxiom = factory.getOWLObjectPropertyDomainAxiom(hacePeliculas, actorClass);
-		OWLObjectPropertyRangeAxiom domainAxiom = factory.getOWLObjectPropertyRangeAxiom(hacePeliculas, actorClass);
-		OWLDataProperty pelis = factory.getOWLDataProperty("pelis");
+		OWLClass actorClass = manager.addClass("Actor");
+		OWLClass peliculaClass = manager.addClass("Pelicula");
+		OWLClass paisClass = manager.addClass("Pais");
+		OWLClass actorFamosoClass = manager.addClass("ActorFamoso");
+
 		OWLDatatype integerDatatype = factory.getIntegerOWLDatatype();
 
-		OWLDataPropertyRangeAxiom pelisRange = factory.getOWLDataPropertyRangeAxiom(pelis, integerDatatype);
+		OWLDatatypeRestriction minRestriction = factory.getOWLDatatypeMinInclusiveRestriction(2);
+		OWLDataProperty haHechoPeliculas = manager.createDataProperty("haHechoPeliculas", actorClass, integerDatatype);
+		manager.createDataProperty("haHechoPeliculas", actorFamosoClass, minRestriction);
 
-		OWLDeclarationAxiom declarationAxiom = factory.getOWLDeclarationAxiom(actorClass);
-		OWLDeclarationAxiom declarationAxiomPelicula = factory.getOWLDeclarationAxiom(peliculaClass);
-		OWLDeclarationAxiom declarationAxiomPais = factory.getOWLDeclarationAxiom(paisClass);
-		OWLOntology ontology = manager.createOntology();
+		manager.createObjectProperty("actuaEn", actorClass, peliculaClass);
+		manager.createObjectProperty("grabadaEn", peliculaClass, paisClass);
+		manager.createObjectProperty("haActuadoEn", actorClass, paisClass);
+//        OWLClassExpression hasPartSomeNose = factory.getOWLObjectSomeValuesFrom(hasPart, nose);
 
-		manager.addAxiom(ontology, rangeAxiom);
-		manager.addAxiom(ontology, domainAxiom);
-		manager.addAxiom(ontology, pelisRange);
-		manager.addAxiom(ontology, declarationAxiom);
-		manager.addAxiom(ontology, declarationAxiomPelicula);
-		manager.addAxiom(ontology, declarationAxiomPais);
+		manager.createSubclass(actorFamosoClass, actorClass);
+//		OWLDataRange intGreaterThan2 = factory.getOWLDatatypeMinInclusiveRestriction(2);
+//		OWLClassExpression actorFamoso = factory.getOWLDataSomeValuesFrom(haHechoPeliculas, intGreaterThan2);
 
-		ArrayList<String> actores = getActores();
-		ArrayList<String> peliculas = getPeliculas();
-		ArrayList<String> paises = getPaises();
-		mappingInstances(actorClass, actores, dbpm, ontology, "actores");
-		mappingInstances(peliculaClass, peliculas, dbpm, ontology, "peliculas");
-		mappingInstances(paisClass, paises, dbpm, ontology, "paises");
+//		manager.createSubclass(actorFamosoClass, actorFamoso);
+
+		ResultSet actores = getData(actoresQuery,dbpedia);
+		ResultSet peliculas = getData(filmQueryString,wikidata);
+		ResultSet paises = getData(countryQuery,mondial);
+		manager.mappingInstances( actores,actorClass, "Actor_1");
+		manager.mappingInstances( peliculas,peliculaClass,"q");
+		manager.mappingInstances( paises,paisClass, "country");
 		for (OWLAxiom ax : ontology.getLogicalAxioms()) {
 			System.out.println(ax);
 		}
-		
-		manager.saveOntology(ontology, IRI.create(file.toURI()));
+
+		manager.saveOntology(directory + File.separator + "ontology.owl");
 
 	}
 }
---
+++ src/main/java/practica_sbc/OntologyManager.java
@@ -0,0 +1,123 @@
+package practica_sbc;
+
+import java.io.File;
+
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDataPropertyDomainAxiom;
+import org.semanticweb.owlapi.model.OWLDataPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLDatatype;
+import org.semanticweb.owlapi.model.OWLDatatypeRestriction;
+import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
+import org.semanticweb.owlapi.model.OWLIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
+import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+import org.semanticweb.owlapi.model.PrefixManager;
+import org.semanticweb.owlapi.util.DefaultPrefixManager;
+
+public class OntologyManager {
+	static final OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+	static final OWLDataFactory factory = manager.getOWLDataFactory();
+	private OWLOntology ontology;
+	private IRI ontologyIRI = IRI.create("http://urjc_sbc.com/owl/");
+	private PrefixManager pm = new DefaultPrefixManager(null, null, this.ontologyIRI.toString());
+
+	public OntologyManager() throws OWLOntologyCreationException {
+		this.ontology = manager.createOntology(this.ontologyIRI);
+	}
+
+	public OWLClass addClass(String name) {
+		OWLClass cl = factory.getOWLClass(name, this.pm);
+		OWLDeclarationAxiom declarationAxiom = factory.getOWLDeclarationAxiom(cl);
+		manager.addAxiom(this.ontology, declarationAxiom);
+		return cl;
+	}
+
+	// Para crear ObjectProperties entre dos clases
+	public OWLObjectProperty createObjectProperty(String name, OWLClass domainClass, OWLClass rangeClass) {
+		OWLObjectProperty property = factory.getOWLObjectProperty(":" + name, this.pm);
+		OWLObjectPropertyDomainAxiom rangeAxiom = factory.getOWLObjectPropertyDomainAxiom(property, domainClass);
+		OWLObjectPropertyRangeAxiom domainAxiom = factory.getOWLObjectPropertyRangeAxiom(property, rangeClass);
+		manager.addAxiom(this.ontology, rangeAxiom);
+		manager.addAxiom(this.ontology, domainAxiom);
+		return property;
+
+	}
+
+	// Para crear DataProperties con restriccion
+	public OWLDataProperty createDataProperty(String name, OWLClass domainClass,OWLDatatypeRestriction restriction) {
+		OWLDataProperty property = factory.getOWLDataProperty(":" + name, this.pm);
+		
+		OWLDataPropertyDomainAxiom domainAxiom = factory.getOWLDataPropertyDomainAxiom(property, domainClass);
+		OWLDataPropertyRangeAxiom rangeAxiom = factory.getOWLDataPropertyRangeAxiom(property, restriction);
+		manager.addAxiom(this.ontology, rangeAxiom);
+		manager.addAxiom(this.ontology, domainAxiom);
+		return property;
+	}
+
+	// Para crear DataProperties sin restriccion
+	public OWLDataProperty createDataProperty(String name, OWLClass domainClass,OWLDatatype type) {
+		OWLDataProperty property = factory.getOWLDataProperty(":" + name, this.pm);
+		
+		OWLDataPropertyDomainAxiom domainAxiom = factory.getOWLDataPropertyDomainAxiom(property, domainClass);
+		OWLDataPropertyRangeAxiom rangeAxiom = factory.getOWLDataPropertyRangeAxiom(property, type);
+		manager.addAxiom(this.ontology, rangeAxiom);
+		manager.addAxiom(this.ontology, domainAxiom);
+		return property;
+	}
+
+	public void createSubclass(OWLClass c1, OWLClassExpression c2) {
+		OWLSubClassOfAxiom ax = factory.getOWLSubClassOfAxiom(c1, c2);
+		manager.addAxiom(this.ontology, ax);
+	}
+	
+	public void createSubclass(OWLClass c1, OWLClass c2) {
+		OWLSubClassOfAxiom ax = factory.getOWLSubClassOfAxiom(c1, c2);
+		manager.addAxiom(this.ontology, ax);
+	}
+	
+	public void mappingInstances(ResultSet results,OWLClass ontologyClass, String field){
+		while (results.hasNext()) {
+			QuerySolution soln = results.nextSolution();
+				OWLIndividual ind = factory.getOWLNamedIndividual(soln.get(field).toString());
+				OWLClassAssertionAxiom ax = factory.getOWLClassAssertionAxiom(ontologyClass, ind);
+				manager.addAxiom(this.ontology, ax);
+		}
+	}
+
+	public OWLOntology getOntology() {
+		return ontology;
+	}
+
+	public void setOntology(OWLOntology ontology) {
+		this.ontology = ontology;
+	}
+
+	public static OWLOntologyManager getManager() {
+		return manager;
+	}
+
+	public static OWLDataFactory getFactory() {
+		return factory;
+	}
+
+	public void saveOntology(String filePath) throws OWLOntologyStorageException {
+		File file = new File(filePath);
+
+		manager.saveOntology(this.ontology, IRI.create(file.toURI()));
+
+	}
+}
