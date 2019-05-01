dc0fbbc8a26756da7d63a4b13b4c186d2b6baf45
diegomendezp <diegomen15@gmail.com>
2019-05-01 18:31:21 +0200

    Check if the ontology is consistent ✨

--- src/main/java/practica_sbc/Reasoner.java
+++ src/main/java/practica_sbc/Reasoner.java
@@ -20,7 +20,11 @@ public class Reasoner {
 	public Reasoner (OWLOntology ontology){
 		OWLReasonerFactory reasonerFactory = new ElkReasonerFactory();
 		this.reasoner = reasonerFactory.createReasoner(ontology);
-		System.out.println(this.reasoner.isConsistent());
+		if(this.reasoner.isConsistent()){
+			System.out.println("The ontology is consistent");
+		} else {
+			System.out.println("The ontology is not consistent");
+		}
 	}
 	
 	public void classifyOntology(){
